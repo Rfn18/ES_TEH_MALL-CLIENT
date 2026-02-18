7c64d552e860b21c998d0f0725a2dcad85992aaf
Rfn18 <rinofaster89@gmail.com>
2026-02-18 22:54:12 +0700

    init: base ui for admin dashboard

--- src/components/ui/CardForm.tsx
+++ src/components/ui/CardForm.tsx
@@ -1,3 +1,5 @@
+import { TableList } from "./TableList";
+
 export const CardForm = () => {
   return (
     <>
@@ -7,7 +9,7 @@ export const CardForm = () => {
           Tambah, edit, atau nonaktifkan item menu
         </p>
       </div>
-      <form className="grid grid-cols-2 grid-rows-2 mt-4">
+      <form className="grid grid-cols-2 grid-rows-2 mt-4 gap-2">
         <div className="flex flex-col text-[#2f524a] gap-2">
           <label
             htmlFor="kd_menu"
@@ -15,27 +17,62 @@ export const CardForm = () => {
           >
             Kode Menu
           </label>
-          <input type="text" name="kd_menu" />
+          <input
+            type="text"
+            name="kd_menu"
+            value={"JNS-260218-002"}
+            disabled
+            className="border border-[#119184]/20 bg-[#f9fafb] w-full rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a]/70"
+          />
         </div>
-        <div>
-          <label htmlFor="nama_menu">Nama Menu</label>
-          <input type="text" name="nama_menu" />
+        <div className="flex flex-col text-[#2f524a] gap-2">
+          <label
+            htmlFor="nama_menu"
+            className="text-[#2f524a] text-sm font-semibold"
+          >
+            Nama Menu
+          </label>
+          <input
+            type="text"
+            name="nama_menu"
+            className="border border-[#119184]/20 bg-[#f9fafb] w-full rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a]"
+          />
         </div>
-        <div>
-          <label htmlFor="jenis_menu">Jenis</label>
-          <select name="jenis_menu">
+        <div className="flex flex-col text-[#2f524a] gap-2">
+          <label
+            htmlFor="jenis_menu"
+            className="text-[#2f524a] text-sm font-semibold"
+          >
+            Jenis
+          </label>
+          <select
+            name="jenis_menu"
+            className="border border-[#119184]/20 bg-[#f9fafb] w-full rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a]"
+          >
             <option value="">Pilih Jenis</option>
             <option value="cup_besar">cup_besar</option>
             <option value="cup_kecil">cup_kecil</option>
             <option value="rasa_rasa">rasa_rasa</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="harga_satuan">Harga</label>
-          <input type="number" name="harga_satuan" />
+        <div className="flex flex-col text-[#2f524a] gap-2">
+          <label
+            htmlFor="harga_satuan"
+            className="text-[#2f524a] text-sm font-semibold"
+          >
+            Harga
+          </label>
+          <input
+            type="number"
+            name="harga_satuan"
+            className="border border-[#119184]/20 bg-[#f9fafb] w-full rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a]"
+          />
         </div>
-        <button>+ simpan</button>
+        <button className="py-2 px-4 bg-[#119184]/70 text-white rounded-lg hover:bg-[#0d7a6d] transition-colors cursor-pointer">
+          + simpan
+        </button>
       </form>
+      <TableList />
     </>
   );
 };
---
+++ src/components/ui/TableList.tsx
@@ -0,0 +1,61 @@
+import { Pencil, Search, Trash } from "lucide-react";
+
+export const TableList = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center gap-4 mt-4">
+        <div className="flex border border-[#119184]/20 bg-[#f9fafb] focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a] w-full rounded-lg px-4 py-2">
+          <Search size={20} className=" text-[#2f524a]/70" />
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Cari menu..."
+            className="w-full bg-transparent focus:outline-none ml-2 text-sm text-[#2f524a]"
+          />
+        </div>
+        <div className="ml-4">
+          <select
+            name="jenis_menu"
+            id="jenis_menu"
+            className="border border-[#119184]/20 bg-[#f9fafb] w-50 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#119184]/50 text-sm text-[#2f524a]"
+          >
+            <option value="">Pilih Jenis</option>
+            <option value="cup_besar">cup_besar</option>
+            <option value="cup_kecil">cup_kecil</option>
+            <option value="rasa_rasa">rasa_rasa</option>
+          </select>
+        </div>
+      </div>
+      <div className="mt-4">
+        <table className="w-full text-left gap-2">
+          <thead>
+            <tr className="border-b border-[#119184]/20 text-sm text-[#2f524a] font-semibold">
+              <th className="p-4">Kode </th>
+              <th className="p-4">Nama Menu</th>
+              <th className="p-4">Jenis</th>
+              <th className="p-4">Harga</th>
+              <th className="p-4">Aksi</th>
+            </tr>
+          </thead>
+          <tbody className=" text-md font-semibold text-[#2f524a]">
+            <tr className="border-b border-[#119184]/20">
+              <td className="p-4">JNS-260218-002</td>
+              <td className="p-4">Es Teh Manis</td>
+              <td className="p-4">cup_besar</td>
+              <td className="p-4">Rp. 5000</td>
+              <td className="p-4">
+                <button className="text-sm text-[#2f524a]px-4 py-2 cursor-pointer">
+                  <Pencil size={16} className="text-[#2f524a]" />
+                </button>
+                <button className="text-sm text-[#f44336] px-4 py-2 ml-2 cursor-pointer">
+                  <Trash size={16} className="text-[#f44336]" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
